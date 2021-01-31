b979da302818e780caeb0dbd3c2960528737a62a
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-31 14:26:02 -0500

    Creados componentes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Inicio from './components/Inicio';
+import Blog from './components/Blog';
+import AcercaDe from './components/AcercaDe';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <header>
-          <h1>Mi Blog Personal</h1>
-          <nav>
-            <Link to="/">Inicio</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/acerca-de">Acerca de</Link>
-          </nav>
-        </header>
+        <Header />
         <main>
-          <Route path="/" exact={true}>
-            <div>
-              <h2>Página de Inicio</h2>
-              <p>Esta es la página principal de nuestro sitio..</p>
-            </div>
-          </Route>
-          <Route path="/blog">
-            <div>
-              <h2>Blog</h2>
-              <ul>
-                <li>Artículo 1</li>
-                <li>Artículo 2</li>
-                <li>Artículo 3</li>
-              </ul>
-            </div>
-          </Route>
-          <Route path="/acerca-de">
-            <div>
-              <h2>Acerca de</h2>
-              <p>Esta es la página Acerca de nuestro sitio..</p>
-            </div>
-          </Route>
+          <Route path="/" exact={true} component={Inicio} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/acerca-de" component={AcercaDe} />
         </main>
       </div>
     </BrowserRouter>
---
+++ src/components/AcercaDe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AcercaDe = () => {
+    return ( 
+        <div>
+            <h2>Acerca de</h2>
+            <p>Esta es la página Acerca de nuestro sitio..</p>
+        </div>
+    );
+}
+
+export default AcercaDe;
