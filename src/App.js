319af12f4166485bf46788b7504d315b151ad2c0
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-31 16:22:42 -0500

    Página Error404 agregada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Inicio from './components/Inicio';
 import Blog from './components/Blog';
 import AcercaDe from './components/AcercaDe';
+import Post from './components/Post';
+import Error404 from './components/Error404';
 
 const ContenedorPrincipal = styled.div`
   padding: 40px;
@@ -26,9 +28,13 @@ function App() {
       <ContenedorPrincipal>
         <Header />
         <Main>
-          <Route path="/" exact={true} component={Inicio} />
-          <Route path="/blog" component={Blog} />
-          <Route path="/acerca-de" component={AcercaDe} />
+          <Switch>
+            <Route path="/" exact={true} component={Inicio} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/post/:id" component={Post} />
+            <Route path="/acerca-de" component={AcercaDe} />
+            <Route component={Error404} />
+          </Switch>
         </Main>
       </ContenedorPrincipal>
     </BrowserRouter>
