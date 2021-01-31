84c0e214022b586ffdbb3b4a79dcefaef2de9120
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-31 15:04:57 -0500

    Agregados etilos con styled Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Inicio from './components/Inicio';
 import Blog from './components/Blog';
 import AcercaDe from './components/AcercaDe';
 
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%auto;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background: #FFF;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <ContenedorPrincipal>
         <Header />
-        <main>
+        <Main>
           <Route path="/" exact={true} component={Inicio} />
           <Route path="/blog" component={Blog} />
           <Route path="/acerca-de" component={AcercaDe} />
-        </main>
-      </div>
+        </Main>
+      </ContenedorPrincipal>
     </BrowserRouter>
   );
 }
