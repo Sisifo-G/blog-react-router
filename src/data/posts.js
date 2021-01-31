d8fa3c9108edbb7e1d960296cf95a3917322091e
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-31 15:31:57 -0500

    Mostrando posts

---
+++ src/data/posts.js
@@ -0,0 +1,8 @@
+
+
+export default [
+    {id: 1, titulo: 'Título del Artículo 1', texto: 'Este es el texto del primer artículo de los posts'},
+    {id: 2, titulo: 'Título del Artículo 2', texto: 'Este es el texto del segundo artículo de los posts'},
+    {id: 3, titulo: 'Título del Artículo 3', texto: 'Este es el texto del tercer artículo de los posts'},
+    {id: 4, titulo: 'Título del Artículo 4', texto: 'Este es el texto del cuarto artículo de los posts'}
+];
